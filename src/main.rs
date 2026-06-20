c818da451de3047b48b1364cc49091169470cb95
UserWithARegularName <segfaultat755@gmail.com>
2026-06-20 09:11:40 +0300

    Connect postgres database

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,54 @@
-use serde::Serialize;
 use axum::{
-    Router,
-    Json,
-    routing::get
+    extract::State,
+    http::StatusCode,
+    routing::get,
+    Json, Router,
 };
+use serde::Serialize;
+use sqlx::{postgres::PgPoolOptions, FromRow, PgPool};
 
-#[derive(Serialize)]
+#[derive(Clone, Serialize, FromRow)]
 struct Data {
     name: String,
     description: String,
-    age: i32
+    age: i32,
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), sqlx::Error> {
+    const DATABASE_URL: &str = "postgresql://user:password@database:5432/axum_database_benchmarking";
+    
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(DATABASE_URL)
+        .await?;
+
     let app = Router::new()
         .route("/greeting", get(greeting))
-        .route("/request", get(request));
+        .route("/request", get(request))
+        .with_state(pool);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+
+    Ok(())
 }
 
 async fn greeting() -> String {
     "Hello, World!".to_string()
 }
 
-async fn request() -> Json<Data> {
-    let data = Data {
-        name: "alice".to_string(),
-        description: "some description".to_string(),
-        age: 24
-    };
+async fn request(State(pool): State<PgPool>) -> Result<Json<Vec<Data>>, StatusCode> {
+    let datas = sqlx::query_as::<_, Data>(
+        "SELECT name, description, age FROM requests"
+    )
+    .fetch_all(&pool)
+    .await
+    
+    .map_err(|e| {
+        eprintln!("Database error: {}", e);
+        StatusCode::INTERNAL_SERVER_ERROR
+    })?;
 
-    Json(data)
+    Ok(Json(datas))
 }
